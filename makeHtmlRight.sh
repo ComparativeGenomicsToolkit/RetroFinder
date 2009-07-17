21d7cc1e175d89dc99fed7119c6b4ec7a56e3b05
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-07-17 16:04:48 +0000

    parameterize scripts to make webpages

--- makeHtmlRight.sh
+++ makeHtmlRight.sh
@@ -1,10 +1,7 @@
 #
 sort -k5,5nr $2 > $2.sort ; mv $2.sort $2
 tawk '{print $4}' $2 > tmp.select
-#echo tawk '{print $4}' $2 to tmp.select
-#echo grep -F -f tmp.select retroMrnaInfo.txt to  $2.txt
 grep -F -f tmp.select retroMrnaInfo.txt > $2.txt
-#~markd/bin/bedToHtmlDir -dir-frame-per 75 -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 /usr/local/apache/htdocs/retro/$3
 ./bedToHtmlDir -dir-frame-per 75 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 /usr/local/apache/htdocs/retro/$3
 count=`wc -l $2 | awk '{print $1}'`
 echo copy $count to /usr/local/apache/htdocs/retro/$3 
