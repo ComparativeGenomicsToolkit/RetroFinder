6747b492e38af7bb715f5d7beee1859cfc01f468
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-08-17 21:58:10 +0000

    remove /tmp from cluster run

--- doBuildpk.sh
+++ doBuildpk.sh
@@ -13,7 +13,7 @@ echo "/cluster/home/baertsch/bin/x86_64/pslPseudo -verbose=5 -minAli=0.98 -nearT
 /cluster/home/baertsch/bin/x86_64/pslPseudo -verbose=5 -minAli=0.98 -nearTop=0.005 -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz > $TMP/pseudo$1.log
 if [ $? == 0 ]; then
 ulimit -a >> $TMP/pseudo$1.log
- /bin/awk '$34 == 1 || $34==-2 {print $0}' $TMP/pseudoMrnaLink$1.txt >/tmp/pseudoGeneLink$1.bed
+ /bin/awk '$34 == 1 || $34==-2 {print $0}' $TMP/pseudoMrnaLink$1.txt > $TMP/pseudoGeneLink$1.bed
  /bin/mv $TMP/pseudo$1.log $LOG/pseudo$1.log
  /bin/rm -f $TMP/pseudoMrnaLink$1.txt.gz
  /bin/gzip -f $TMP/pseudoMrnaLink$1.txt
