c9e9d27a30923b21ff5b38896d04da672a84e1fc
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-08-19 01:55:16 +0000

    fix hard coded directory add parameters

--- makeHtmlRightDb.sh
+++ makeHtmlRightDb.sh
@@ -1,7 +1,10 @@
 #
 set -beEu -o pipefail
 source $1
+bed=$2
+dir=$3
+name=$4
 mkdir -p $ROOTDIR/retro/$3
-echo "<td>$4</td>" >> $ROOTDIR/retro/$3/../index.html
-for db in `echo $SPECIES` ; do mkdir -p ../../$db/$EXPDIR ; pushd ../../$db/$EXPDIR ; $SCRIPT/makeHtmlSpecies.sh $db $2 $3 $4 $SCRIPT; popd ; done
-echo "</TR>" >> $ROOTDIR/retro/$3/../index.html
+echo "<td>$4</td>" >> $ROOTDIR/retro/$dir/../index.html
+for db in `echo $SPECIES` ; do mkdir -p ../../$db/$EXPDIR ; pushd ../../$db/$EXPDIR ; $SCRIPT/makeHtmlSpecies.sh $db $bed $dir $name $SCRIPT $ROOTDIR/retro ; popd ; done
+echo "</TR>" >> $ROOTDIR/retro/$dir/../index.html
--- makeHtmlSpecies.sh
+++ makeHtmlSpecies.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-echo "dir = $3/$1"
-echo `pwd`
+db=$1
+bed=$2
+dir=$3
+name=$4
+script=$5
+webdir=$6
+echo "writing html into $dir/$db"
 sort -k5,5nr $2 > $2.sort ; mv $2.sort $2
 tawk '{print $4}' $2 > tmp.select
 echo "makeHtmlSpecies.sh bin $5 file $2 "
 pwd
 echo "grep -F -f tmp.select retroMrnaInfo.txt output $2.txt"
 grep -F -f tmp.select retroMrnaInfo.txt > $2.txt
-echo "$5/bedToHtmlDir -dir-frame-per 75 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 /usr/local/apache/htdocs/retro/$3/$1"
-$5/bedToHtmlDir -dir-frame-per 75 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 /usr/local/apache/htdocs/retro/$3/$1
+echo "$script/bedToHtmlDir -page-size 21 -dir-frame-per 60 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 $webdir/$3/$1"
+$script/bedToHtmlDir -page-size 21 -dir-frame-per 60 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 $webdir/$3/$1
 count=`wc -l $2 | awk '{print $1}'`
-echo copy $count to /usr/local/apache/htdocs/retro/$3 
-echo "<TD><a href=../$3/$1>"$count"</a></Td>" >> /usr/local/apache/htdocs/retro/$3/../index.html
+echo copy $count to $webdir/$3/../index.html
+echo "<TD><a href=../$3/$1>"$count"</a></Td>" >> $webdir/$3/../index.html
