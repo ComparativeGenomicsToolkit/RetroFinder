73af3edf9c1196f8f6123156ef617b1ccf669308
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-08-19 02:20:45 +0000

    add selectByID instead of grep

--- makeHtmlSpecies.sh
+++ makeHtmlSpecies.sh
@@ -5,15 +5,14 @@ dir=$3
 name=$4
 script=$5
 webdir=$6
-echo "writing html into $dir/$db"
-sort -k5,5nr $2 > $2.sort ; mv $2.sort $2
-tawk '{print $4}' $2 > tmp.select
-echo "makeHtmlSpecies.sh bin $5 file $2 "
-pwd
-echo "grep -F -f tmp.select retroMrnaInfo.txt output $2.txt"
-grep -F -f tmp.select retroMrnaInfo.txt > $2.txt
-echo "$script/bedToHtmlDir -page-size 21 -dir-frame-per 60 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 $webdir/$3/$1"
-$script/bedToHtmlDir -page-size 21 -dir-frame-per 60 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 $webdir/$3/$1
-count=`wc -l $2 | awk '{print $1}'`
-echo copy $count to $webdir/$3/../index.html
-echo "<TD><a href=../$3/$1>"$count"</a></Td>" >> $webdir/$3/../index.html
+echo "Writing $name html into $dir/$db "
+sort -k5,5nr $bed > $bed.sort ; mv $bed.sort $bed
+#tawk '{print $4}' $bed > tmp.select
+#echo "grep -F -f tmp.select retroMrnaInfo.txt output $bed.txt"
+#grep -F -f tmp.select retroMrnaInfo.txt > $bed.txt
+$script/selectById 4 $bed 1 retroMrnaInfo.txt > $bed.txt
+echo "$script/bedToHtmlDir -page-size 21 -dir-frame-per 60 -dir-right -context-bases 500 -labels $bed.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $db $bed $webdir/$dir/$db"
+$script/bedToHtmlDir -page-size 21 -dir-frame-per 60 -dir-right -context-bases 500 -labels $bed.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $db $bed $webdir/$dir/$db
+count=`wc -l $bed | awk '{print $1}'`
+echo copy $count to $webdir/$dir/../index.html
+echo "<TD><a href=../$dir/$db>"$count"</a></Td>" >> $webdir/$dir/../index.html
