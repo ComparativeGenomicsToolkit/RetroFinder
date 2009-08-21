264eee04b0cb7f57df3b270f32bf0f60c5c3c9ff
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-08-21 21:19:37 +0000

    add column for ancient vs recent retros remove hardcoded script path

--- ucscRetroStep6.sh
+++ ucscRetroStep6.sh
@@ -4,7 +4,7 @@ source $1
 DEF=$1
 echo "-------- script ucscRetroStep6.sh make retro webpages for $SPECIES -------------------"
 #extract extra columns for html pages
-/hive/users/hartera/GencodeWG/pseudogenes/retroFinder/baertsch/scripts/makeRetroExtraAttr.sh $DEF $OUTDIR/$EXPDIR
+$SCRIPT/makeRetroExtraAttr.sh $DEF $OUTDIR/$EXPDIR
 for i in `echo $SPECIES` ; do echo "make html $i"; done
 
 cp $DEF $OUTDIR/$EXPDIR
@@ -13,10 +13,11 @@ cd $OUTDIR/$EXPDIR
 mkdir -p $ROOTDIR/retro/shuffle2009
 cp $SCRIPT/header.html $ROOTDIR/retro/shuffle2009/index.html
 echo "<TR><TH>data set</TH>" >> $ROOTDIR/retro/shuffle2009/index.html
-for db in `echo $SPECIES` ; do echo "<TH>$db</TH>" >> $ROOTDIR/retro/shuffle2009/index.html ; done
+for db in `echo $SPECIES` ; do echo "<TH>$db Ancient</TH><TH>$db Recent</TH>" >> $ROOTDIR/retro/shuffle2009/index.html ; done
 echo "</TR>" >> $ROOTDIR/retro/shuffle2009/index.html
 echo "</THEAD><TBODY>" >> $ROOTDIR/retro/shuffle2009/index.html
 echo "$SCRIPT/makeHtmlRightDb.sh $DEF pseudoRefGeneCds.bed shuffle2009/shuffle refSeqShuffle;"
+$SCRIPT/makeHtmlRightDb.sh $DEF retroShuffleGood.bed shuffle2009/shuffleGood shuffleGood;
 $SCRIPT/makeHtmlRightDb.sh $DEF pseudoRefGeneCds.bed shuffle2009/shuffle refSeqShuffle;
 $SCRIPT/makeHtmlRightDb.sh $DEF pseudoRefGeneCds50.bed shuffle2009/shuffle50 refSeq50%Shuffle;
 $SCRIPT/makeHtmlRightDb.sh $DEF shuffleEns.bed shuffle2009/shuffleEnd shuffleEns;
@@ -30,7 +31,7 @@ echo "</html>" >> $ROOTDIR/retro/shuffle2009/index.html
 mkdir -p $ROOTDIR/retro/type1_2009
 cp $SCRIPT/header.html $ROOTDIR/retro/type1_2009/index.html
 echo "<TR><TH>data set</TH>" >> $ROOTDIR/retro/type1_2009/index.html
-for db in `echo $SPECIES` ; do echo "<TH>$db</TH>" >> $ROOTDIR/retro/type1_2009/index.html ; done
+for db in `echo $SPECIES` ; do echo "<TH>$db Ancient</TH><TH>$db Recent</TH>" >> $ROOTDIR/retro/type1_2009/index.html ; done
 echo "</TR>" >> $ROOTDIR/retro/type1_2009/index.html
 echo "</THEAD><TBODY>" >> $ROOTDIR/retro/type1_2009/index.html
 $SCRIPT/makeHtmlRightDb.sh $DEF pseudoExpressedAll.bed type1_2009/expressedAll510 expressedAll510;
