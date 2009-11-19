2b09e761d8c21454e3a10c584a7e992cd68177a7
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-11-19 23:58:20 +0000

    change webdir to ~/public_html

--- makeHtmlRight.sh
+++ makeHtmlRight.sh
@@ -2,7 +2,7 @@
 sort -k5,5nr $2 > $2.sort ; mv $2.sort $2
 tawk '{print $4}' $2 > tmp.select
 grep -F -f tmp.select retroMrnaInfo.txt > $2.txt
-./bedToHtmlDir -dir-frame-per 75 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 /usr/local/apache/htdocs/retro/$3
+bedToHtmlDir -dir-frame-per 75 -dir-right -context-bases 500 -labels $2.txt -label-tsv retroMrnaInfo.lab -browser-url http://hgwdev-baertsch.cse.ucsc.edu $1 $2 ~/public_html/retro/$3
 count=`wc -l $2 | awk '{print $1}'`
-echo copy $count to /usr/local/apache/htdocs/retro/$3 
-echo "<TR><TH>"$4"<TD><a href=../$3>"$count"</a></TR>" >> /usr/local/apache/htdocs/retro/$3/../index.html
+echo copy $count to ~/public_html/retro/$3 
+echo "<TR><TH>"$4"<TD><a href=../$3>"$count"</a></TR>" >> ~/public_html/retro/$3/../index.html
