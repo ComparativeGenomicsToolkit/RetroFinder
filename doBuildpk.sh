d48e3e05eb26781be3fc502791a6f7a9aa90b945
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-11-21 08:34:26 +0000

    add pid to temp directory to prevent conflicts increase memory quota for larget net files

--- doBuildpk.sh
+++ doBuildpk.sh
@@ -3,12 +3,12 @@ set -beEu -o pipefail
 SEQ=$1
 source $2
 CACHE=$OUTDIR
-TMP=/scratch/retro
+TMP=/scratch/tmp/retro.$$
 mkdir -p $TMP
 cd $TMP
 rm -f $LOG/pseudo$1.log
-ulimit -d 2200000
-ulimit -v 2200000
+ulimit -d 2800000
+ulimit -v 2800000
 echo "/cluster/home/baertsch/bin/x86_64/pslPseudo -verbose=5 -minAli=0.98 -nearTop=0.005 -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz"
 /cluster/home/baertsch/bin/x86_64/pslPseudo -verbose=5 -minAli=0.98 -nearTop=0.005 -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz > $TMP/pseudo$1.log
 if [ $? == 0 ]; then
