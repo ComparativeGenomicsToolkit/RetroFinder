c4db8a7ec5fb8bef45c769ffdfa3d03cce20ff1d
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-11-23 01:07:02 +0000

    remove temp files

--- doBuildpk.sh
+++ doBuildpk.sh
@@ -23,6 +23,7 @@ ulimit -a >> $TMP/pseudo$1.log
  /bin/gzip $TMP/pseudo$1.axt
 #/bin/cp -fp $TMP/pseudoMrnaLink$1.txt.gz $RESULT
  /bin/mv $TMP/pseudo$1.axt.gz $RESULT/axt
+ /bin/rm -rf $TMP
 
 
 # /bin/awk '{OFS="	";print $14,$16,$17,$10,$1*3-$2,$9}' pseudoMrna.psl > pseudoMrna.txt
