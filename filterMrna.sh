7e72420f54fcb196376e310e73d19785c27dac29
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-11-27 22:38:00 +0000

    fix incorrect file names for mrnaNoversion.2bit

--- filterMrna.sh
+++ filterMrna.sh
@@ -14,10 +14,9 @@ awk 'NF > 1 && $3 > 1{print $1}' all_mrna.stats  > all_mrna.doublehit.list
 pslSelect -queries=all_mrna.doublehit.list all_mrna.qName.psl stdout | grep -v "_hap" |grep -v random |sort -k10,10> all_mrna_double.sort.psl
 cut -f 10 all_mrna_double.sort.psl |sort |uniq > all_mrna.doublehit.list
 $SCRIPT/selectById -not 1 all_mrna.doublehit.list 10 all_mrna.psl > all_mrna_kept.psl
-faToTwoBit -stripVersion all_mrna.fa all_mrna.2bit
-rm -f all_mrna.fa
+faToTwoBit -stripVersion $MRNABASE/mrna.fa mrnaNoversion.2bit
 
-pslCDnaGenomeMatch all_mrna_double.sort.psl S1.len all_mrna.2bit /cluster/data/$DB/nib all_mrna_double.filter.psl -score=all_mrna_double.score -bedOut=all_mrna_double.bed  -verbose=3 -minDiff=5 > all.log 
+pslCDnaGenomeMatch all_mrna_double.sort.psl S1.len mrnaNoversion.2bit /cluster/data/$DB/nib all_mrna_double.filter.psl -score=all_mrna_double.score -bedOut=all_mrna_double.bed  -verbose=3 -minDiff=5 > all.log 
 cat all_mrna_kept.psl all_mrna_double.filter.psl > all_mrnaFiltered.psl
 gzip all_mrnaFiltered.psl
 
