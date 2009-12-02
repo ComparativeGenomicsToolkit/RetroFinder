a27c5592aa8d94b88e71dabf7463d8cbf9068681
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-12-02 04:30:02 +0000

    Added database variale to file name.

--- makeRetroExtraAttr.sh
+++ makeRetroExtraAttr.sh
@@ -6,27 +6,22 @@ source $1
 cd $2
 echo "-------- script makeRetroExtraAttr.sh ------------"
 echo "extract genbank names"
-SQLNAME="select gb.acc, n.name from gbCdnaInfo gb, geneName n, organism o where gb.geneName = n.id and type = 'mRNA' and organism = o.id and o.name = '$GENOMENAME'"
+SQLNAME="select gb.acc, n.name , p.name from gbCdnaInfo gb, geneName n, organism o , productName p where gb.geneName = n.id and type = 'mRNA' and organism = o.id and o.name = '$GENOMENAME' and productName = p.id"
 echo "SQL=" $SQLNAME
 hgsql $DB -N -B -e "$SQLNAME" > genBankName.txt
 echo "drop rbGenBankName"
 hgsql $DB -e "drop table if exists rbGenBankName "
 echo "create rbGenBankName"
-hgsql $DB -e "create table rbGenBankName (acc varchar(255), name varchar(255), PRIMARY KEY(acc));"
+hgsql $DB -e "create table rbGenBankName (acc varchar(255), name varchar(255), product varchar(255), PRIMARY KEY(acc));"
 echo "load rbGenBankName"
 hgsql $DB -e "load data local infile 'genBankName.txt' into table rbGenBankName"
 
-#hgsql $DB -N -B -e "select r.name, r.name, score, concat(f.geneName,',',replace(n.name,'n/a','')), r.type, overlapRhesus, overlapMouse, overlapDog, overName, r.blockCount, conservedSpliceSites, exonCover, coverage, milliBad from gbCdnaInfo cd, geneName n,  retroMrnaInfo r left outer join refFlat f on r.refSeq = f.name where cd.geneName = n.id and cd.acc = kgName " > retroMrnaInfo.txt
 echo "extract retroMrnaInfo.txt"
-echo "hgsql $DB -N -B -e select r.name, r.name, score, n.name,r.type, retroExonCount, overlapRhesus, overlapMouse, overlapDog, overName, r.blockCount, conservedSpliceSites, exonCover, coverage, milliBad from $TABLE r left outer join rbGenBankName n on r.refSeq = n.acc " 
-hgsql $DB -N -B -e "select r.name, r.name, score, n.name,r.type, retroExonCount, overlapRhesus, overlapMouse, overlapDog, overName, r.blockCount, conservedSpliceSites, exonCover, coverage, milliBad from $TABLE  r left outer join rbGenBankName n on r.refSeq = n.acc " > retroMrnaInfo.txt
-#hgsql $DB -N -B -e "select r.name, r.name, score, replace(n.name,'n/a',''), r.type, overlapRhesus, overlapMouse, overlapDog, overName, r.blockCount, conservedSpliceSites, exonCover, coverage, milliBad from $TABLE  r , rbGenBankName n where r.refSeq = n.acc " > retroMrnaInfo.txt
+hgsql $DB -N -B -e "select r.name, r.name, score, n.name,r.type, retroExonCount, overlapRhesus, overlapMouse, overlapDog, overName, r.blockCount, conservedSpliceSites, exonCover, coverage, milliBad, n.product from $TABLE  r left outer join rbGenBankName n on r.refSeq = n.acc " > retroMrnaInfo.txt
 pwd
-hgsql $DB -B -e "select name, name, score, refSeq as parent, type, retroExonCount as Exons, overlapRhesus as Rhesus , overlapMouse as Mus, overlapDog as Dog, overName as exp, blockCount, conservedSpliceSites as consSS, exonCover, coverage, milliBad from $TABLE  limit 1" > retroMrnaInfo.hg18.lab
+hgsql $DB -B -e "select name, name, score, refSeq as parent, type, retroExonCount as Exons, overlapRhesus as Rhesus , overlapMouse as Mus, overlapDog as Dog, overName as exp, blockCount, conservedSpliceSites as consSS, exonCover, coverage, milliBad, name as product from $TABLE  limit 1" > retroMrnaInfo.$DB.lab
 #cp retroMrnaInfo.hg18.lab retroMrnaInfo.$DB.lab
-hgsql $DB -B -e "select name, name, score, refSeq as parent, type, retroExonCount as Exons, overlapRhesus as Rat , overlapMouse as Human, overlapDog as Dog, overName as exp, blockCount, conservedSpliceSites as consSS, exonCover, coverage, milliBad from $TABLE  limit 1" > retroMrnaInfo.mm9.lab
-wc -l retroMrnaInfo.txt retroMrnaInfo.mm9.lab retroMrnaInfo.hg18.lab
+hgsql $DB -B -e "select name, name, score, refSeq as parent, type, retroExonCount as Exons, overlapRhesus as Rat , overlapMouse as Human, overlapDog as Dog, overName as exp, blockCount, conservedSpliceSites as consSS, exonCover, coverage, milliBad , name as product from $TABLE  limit 1" > retroMrnaInfo.mm9.lab
+wc -l retroMrnaInfo.txt retroMrnaInfo.mm9.lab retroMrnaInfo.$DB.lab
 
-#hgsql $DB -B -N -e "select chrom, chromStart, chromEnd, name, score, strand from $TABLE  where (overlapDog < 40 and overlapMouse < 40 ) or (overlapRhesus < 50 and (overlapDog < 40 and overlapMouse > 40) or (overlapMouse <40 and overlapDog > 40))" > retroAncient.bed
-#wc -l retroAncient.bed
 echo "-------- END script makeRetroExtraAttr.sh ------------"
