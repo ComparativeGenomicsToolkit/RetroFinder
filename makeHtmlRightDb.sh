29aff2f0fda3186639a77547e208e21c312a8b7e
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-12-02 18:14:29 +0000

    check for input file before writing html, skip if input file missing or empty

--- makeHtmlRightDb.sh
+++ makeHtmlRightDb.sh
@@ -4,7 +4,11 @@ source $1
 bed=$2
 dir=$3
 name=$4
-mkdir -p $ROOTDIR/retro/$3
-echo "<td>$4</td>" >> $ROOTDIR/retro/$dir/../index.html
-for db in `echo $SPECIES` ; do mkdir -p ../../$db/$EXPDIR ; pushd ../../$db/$EXPDIR ; $SCRIPT/makeHtmlSpecies.sh $db $bed $dir $name $SCRIPT $ROOTDIR/retro ; popd ; done
-echo "</TR>" >> $ROOTDIR/retro/$dir/../index.html
+if [[ -s $bed ]] ; then
+    if [[ -s ${bed%%.bed}.ancient.bed ]] ; then
+        mkdir -p $ROOTDIR/retro/$3
+        echo "<td>$4</td>" >> $ROOTDIR/retro/$dir/../index.html
+        for db in `echo $SPECIES` ; do mkdir -p ../../$db/$EXPDIR ; pushd ../../$db/$EXPDIR ; $SCRIPT/makeHtmlSpecies.sh $db $bed $dir $name $SCRIPT $ROOTDIR/retro ; popd ; done
+        echo "</TR>" >> $ROOTDIR/retro/$dir/../index.html
+    fi
+fi
