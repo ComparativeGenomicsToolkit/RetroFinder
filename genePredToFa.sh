357feb6f04587b69123752a08247e2d09bf5fe66
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2009-12-14 00:39:14 +0000

    remove psl Header from output

--- genePredToFa.sh
+++ genePredToFa.sh
@@ -3,4 +3,4 @@ source $1
 chr=$2
 getRnaPred -genomeSeqs=$GENOME/$DB/$DB.2bit -cdsUpper $DB ../transMapAlnMRna.gp $chr mrnaCds.${chr}.fa
 tr acgt ACGT < mrnaCds.${chr}.fa > mrna.${chr}.fa
-blat $GENOME/$DB/$DB.2bit mrna.${chr}.fa ../mrna.psl/${chr}.psl -q=rna -fine -mask=lower -ooc=$GENOME/$DB/11.ooc
+blat $GENOME/$DB/$DB.2bit mrna.${chr}.fa ../mrna.psl/${chr}.psl -noHead -q=rna -fine -mask=lower -ooc=$GENOME/$DB/11.ooc
