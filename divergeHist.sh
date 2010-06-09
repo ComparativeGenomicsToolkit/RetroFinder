b193c346a02552775e443ba1fbdfc87b43153ed2
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2010-06-09 20:28:58 +0000

    add chrX

--- divergeHist.sh
+++ divergeHist.sh
@@ -2,20 +2,20 @@
 #calc divergence histograms 
 #$1 = DEF file
 source $1
-cd /hive/users/baertsch/retro/$DB
-hgsql $DB -e "select (1000-millibad)/10, type from ${TABLE}" |sort -n>diverge.txt
-cut -f 35 exp/age/retroKgCoding.bed > divergeKg.txt
-grep pseudogene diverge.txt |awk '{print $1}' > div.pseudo.txt
-grep pseudogene diverge.txt |awk '{print $1}' > div.pseudoweak.txt
-grep express diverge.txt |grep -v strong |grep -v shuffle |awk '{print $1}' >> div.pseudoweak.txt
-grep express diverge.txt |awk '{print $1}' > div.express.txt
-grep strong diverge.txt |awk '{print $1}' > div.strong.txt
-grep shuffle diverge.txt |awk '{print $1}' >> div.strong.txt
-cat divergeKg.txt |awk '{print $1}' > div.strong.kg.txt
-grep express diverge.txt|grep -v strong |awk '{print $1}' > div.weak.txt
+#cd /hive/users/baertsch/retro/$DB
+hgsql $DB -e "select (1000-millibad)/10, type, chrom from ${TABLE}" |awk '$3=="chrX"{print $1,$2,$3}$3!="chrX"{print $1,$2,"auto"}' | sort -n>diverge.txt
+tawk '{print (1000-$35)/10,$1}' exp/age/retroKgCoding.bed > divergeKg.txt
+grep pseudogene diverge.txt |awk '{print $1,$3}' > div.pseudo.txt
+grep pseudogene diverge.txt |awk '{print $1,$3}' > div.pseudoweak.txt
+grep express diverge.txt |grep -v strong |grep -v shuffle |awk '{print $1,$3}' >> div.pseudoweak.txt
+grep express diverge.txt |awk '{print $1,$3}' > div.express.txt
+grep strong diverge.txt |awk '{print $1,$3}' > div.strong.txt
+grep shuffle diverge.txt |awk '{print $1,$3}' >> div.strong.txt
+cat divergeKg.txt |awk '{print $1,$3}' > div.strong.kg.txt
+grep express diverge.txt|grep -v strong |awk '{print $1,$3}' > div.weak.txt
 
-echo "pdf('div.${DB}.pseudo.pdf')" >> div.R
-echo "op <- par(mfcol=c(2, 2))" > div.R
+echo "pdf('div.${DB}.pseudo.pdf')" > div.R
+echo "op <- par(mfcol=c(2, 2))" >> div.R
 echo "pseudo<-scan('div.pseudo.txt')">> div.R
 echo "express<-scan('div.express.txt')" >> div.R
 echo "strong<-scan('div.strong.txt')" >> div.R
@@ -39,7 +39,7 @@ echo "pdf('div.${DB}.strong.pdf')" >> div.R
 echo "op <- par(mfcol=c(1, 3))" >> div.R
 echo "pseudoweak.${DB}<-pseudoweak[pseudoweak<=$MAXDIVERGENCE]" >> div.R
 echo "length(pseudoweak.${DB})">>div.R
-echo "hp<-hist(pseudoweak.${DB},freq=TRUE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} non-expressed retrocopies', main='Not Expressed ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
+echo "hp<-hist(pseudoweak.${DB},freq=TRUE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),ylim=range(0,$YLIM1),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} non-expressed retrocopies', main='Not Expressed ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
 #echo "hp<-hist(pseudogene.${DB},freq=FALSE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),ylim=range(0,$YLIM),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} non-expressed retrocopies', main='Not Expressed ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
 echo "text(hp\$mids, hp\$density, hp\$counts, adj=c(.5, -.5), col='blue3')" >> div.R
 
@@ -47,10 +47,11 @@ echo "text(hp\$mids, hp\$density, hp\$counts, adj=c(.5, -.5), col='blue3')" >> d
 echo "expressStrong.${DB}<-strong[strong<=$MAXDIVERGENCE]" >> div.R
 echo "expressKg.${DB}<-strongKg[strongKg<=$MAXDIVERGENCE]" >> div.R
 echo "length(expressStrong.${DB})">>div.R
-echo "hs<-hist(expressStrong.${DB},freq=TRUE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} expressed retrocopies', main='Expressed ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
+echo "hs<-hist(expressStrong.${DB},freq=TRUE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),ylim=range(0,$YLIM2),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} expressed retrocopies', main='Expressed ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
 #echo "hs<-hist(expressStrong.${DB},freq=FALSE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),ylim=range(0,$YLIM),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} expressed retrocopies', main='Expressed ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
 echo "text(hs\$mids, hs\$density, hs\$counts, adj=c(.5, -.5), col='green3')" >> div.R
-echo "hk<-hist(expressKg.${DB},freq=TRUE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} known retrogene', main='RetroGenes ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
+echo "hk<-hist(expressKg.${DB},freq=TRUE,breaks=c(${BREAKS}), xlim=range(0,$XLIM),ylim=range(0,$YLIM2),xlab='Range of substitution level (percent)',ylab='Fraction of ${GENOMENAME} known retrogene', main='RetroGenes ${GENOMENAME}',sub='each bar represents ~25MYA')" >> div.R
+echo "text(hk\$mids, hk\$density, hk\$counts, adj=c(.5, -.5), col='red3')" >> div.R
 echo "par(op)">>div.R
 echo "dev.off()">> div.R
 
