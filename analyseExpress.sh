bb6f48f96fb60d051874528dc2436dee4377a6d2
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2011-10-24 15:42:27 +0000

    check if knownGene exists, other small changes

--- analyseExpress.sh
+++ analyseExpress.sh
@@ -4,25 +4,52 @@ source $1
 echo '-------- script analyseExpress.sh -------------------'
 #expression according to knownGene
 hgsql $PDB -N -B -e "select * from hgncXref " > hgncXref.tab
+if [[ $GENE1 == "knownGene" ]] 
+then
 hgsql $DB -N -B -e "select kg.* from knownGene kg, kgTxInfo i where kg.name = i.name and category = 'coding'" > kgCoding.gp
 hgsql $DB -N -B -e "select kg.* from knownGene kg, kgTxInfo i where kg.name = i.name and category = 'noncoding'" > kgNoncoding.gp
 hgsql $DB -N -B -e "select kg.* from knownGene kg, kgTxInfo i where kg.name = i.name and category = 'nearcoding'" > kgNearcoding.gp
 hgsql $DB -N -B -e "select kg.* from knownGene kg, kgTxInfo i where kg.name = i.name and category = 'antisense'" > kgAntisense.gp
 for i in Coding Noncoding Nearcoding Antisense ; do overlapSelect kg${i}.gp ../$TABLE.bed retroKg${i}.bed; $SPLITBYAGE retroKg${i}.bed retroKg${i}.ancient.bed retroKg${i}.recent.bed; done
+zcat ../knownGene.tab.gz |sort > ../knownGene.sort.gp
+hgsql $DB -N -B -e "select distinct kgID from kgSpAlias, uniProt.proteinEvidence where spId = acc and proteinEvidenceType <=1" |sort> kgProtEvidence.id
+hgsql $DB -N -B -e "select distinct kgID from kgSpAlias, uniProt.proteinEvidence where spId = acc and proteinEvidenceType <=2" |sort> kgTransEvidence.id
+$SCRIPT/selectById -tsv 1 kgProtEvidence.id 1 ../knownGene.sort.gp > knownGeneProt.gp
+$SCRIPT/selectById -tsv 1 kgTransEvidence.id 1 ../knownGene.sort.gp > knownGeneTrans.gp
+
+overlapSelect knownGeneProt.gp ../${GENE1}.cds.bed knownGeneProtCds.bed -selectFmt=genePred
+overlapSelect knownGeneTrans.gp ../${GENE1}.cds.bed knownGeneTransCds.bed -selectFmt=genePred
+overlapSelect knownGeneProtCds.bed ../$TABLE.bed ucscRetroProtein.bed
+overlapSelect knownGeneTransCds.bed ../$TABLE.bed ucscRetroTranscript.bed
+fi
+
 echo "Exon Shuffling"
 # exon shuffling
+cat ../$TABLE.bed ../retroMrnaInfoZnf.bed > ucscRetroInfoZnf.bed
+
 overlapSelect ../${GENE1}.multiCds.bed ../$TABLE.bed pseudo${GENE1}Cds.bed
-overlapSelect ../${GENE1}.multiCds.bed ../retroMrnaInfo.12.bed -statsOutput pseudo${GENE1}Cds.out
-overlapSelect ../${GENE1}.multiCds.bed ../retroMrnaInfo.12.bed pseudo${GENE1}Cds50.bed -overlapThreshold=0.50
+overlapSelect ../${GENE1}.multiCds.bed ../$TABLE.bed -statsOutput pseudo${GENE1}Cds.out
+overlapSelect ../${GENE1}.multiCds.bed ../$TABLE.bed pseudo${GENE1}Cds50.bed -overlapThreshold=0.50
 overlapSelect ../${GENE2}.multiCds.bed ../$TABLE.bed pseudo${GENE2}Cds.bed
-overlapSelect ../${GENE2}.multiCds.bed ../retroMrnaInfo.12.bed -statsOutput pseudo${GENE2}Cds.out
-overlapSelect ../${GENE2}.multiCds.bed ../retroMrnaInfo.12.bed pseudo${GENE2}Cds50.bed -overlapThreshold=0.50
+overlapSelect ../${GENE2}.multiCds.bed ../$TABLE.bed -statsOutput pseudo${GENE2}Cds.out
+overlapSelect ../${GENE2}.multiCds.bed ../$TABLE.bed pseudo${GENE2}Cds50.bed -overlapThreshold=0.50
 overlapSelect -selectFmt=genePred ../${GENE2}.tab.gz pseudo${GENE2}Cds.bed shuffleEns.bed
 overlapSelect -selectFmt=genePred ../${GENE2}.multiCDSExon.genePred pseudo${GENE2}Cds.bed shuffleEnsMulti.bed
-
+overlapSelect pseudo${GENE1}Cds.bed ../pseudoEstAll.bed pseudoEstAllNotShuffle.bed -nonOverlapping
 for i in ${GENE1}Cds ${GENE2}Cds ${GENE1}Cds50 ${GENE2}Cds50 ; do $SPLITBYAGE pseudo${i}.bed pseudo${i}.ancient.bed pseudo${i}.recent.bed ; done
+
+if [[ $GENE1 == "knownGene" ]] 
+then
+overlapSelect knownGeneProt.gp pseudo${GENE1}Cds.bed pseudo${GENE1}ProtCds.bed
+overlapSelect knownGeneProt.gp pseudo${GENE2}Cds.bed pseudo${GENE2}ProtCds.bed
+for i in ${GENE1}ProtCds ${GENE2}ProtCds; do $SPLITBYAGE pseudo${i}.bed pseudo${i}.ancient.bed pseudo${i}.recent.bed ; done
+fi
+
 ls -l pseudo${GENE1}*.bed
 wc -l pseudo${GENE1}*.bed
+cut -f 4 pseudo${GENE1}Cds.bed > shuffle.list
+awk '{print "update '$TABLE' set type = \"expressed shuffle\" where name = \""$1"\" ;"}' shuffle.list> updateShuffle.sql
+hgsql $DB < updateShuffle.sql
 
 # expression analysis based on mrna and est overlap
 # grab results of cluster job
@@ -65,14 +92,25 @@ $SCRIPT/selectById -tsv 1 mrnaEst10.id 4 ../$TABLE.bed >  pseudoEst10Mrna.bed
 $SCRIPT/selectById -tsv 1 est5.id 4 ../$TABLE.bed > pseudoEst5.bed
 $SCRIPT/selectById -tsv 1 est10.id 4 ../$TABLE.bed > pseudoEst10.bed
 overlapSelect pseudoEst10Mrna.bed pseudo${GENE2}Cds.bed  est10MrnaRefSeq.bed
+
+overlapSelect retroKgCoding.bed pseudoEst5Mrna.bed pseudoEst5MrnaNotKg.bed -nonOverlapping
+overlapSelect retroKgCoding.bed pseudoEst10Mrna.bed pseudoEst10MrnaNotKg.bed -nonOverlapping
+overlapSelect retroKgCoding.bed pseudoEst5AndMrna.bed pseudoEst5AndMrnaNotKg.bed -nonOverlapping
+
+#sort -k2,2nr estCoverage.out |head -50|awk '{print $1}' > top50
+#echo $SCRIPT/selectById -tsv 1 top50 4 ../$TABLE.bed   top50.bed
+#$SCRIPT/selectById -tsv 1 top50 4 ../$TABLE.bed  > top50.bed
+
+for i in ucscRetroProtein ucscRetroTranscript ; do $SPLITBYAGE ${i}.bed ${i}.ancient.bed ${i}.recent.bed ; done
 echo "expressed retro stats"
 wc -l *.id |sort -n
 overlapSelect ../${GENE2}.tab.gz ../$TABLE.bed pseudo${GENE2}.bed -selectFmt=genePred
 
 #regenerate gene predictions from expressed retros
 
-#set with 1 or more EST (tight)
-tawk '$2>50{$2=$2-50; $3=$3+50;print $0}$2<=50{$3=$3+50;print $0}' ../pseudoEstAll.bed > pseudoEstAll.bed
+#set with 1 or more EST (tight) not shuffle events
+#look for orf in window 200 bp around retro
+tawk '$2>200{$2=$2-200; $3=$3+200;print $0}$2<=200{$3=$3+200;print $0}' pseudoEstAllNotShuffle.bed > pseudoEstAll.bed
 echo "orfBatch $DB pseudoEstAll.bed pseudoEstAll.out pseudoEstAll.gp to borfEst.out"
 orfBatch $DB pseudoEstAll.bed pseudoEstAll.out pseudoEstAll.gp > borfEst.out
 echo gene-check  -nib-dir $NIB pseudoEstAll.gp checkEst.rdb
@@ -88,7 +126,8 @@ echo "hgsql $DB updateExp.sql"
 hgsql $DB < updateExp.sql
 
 #set with 5 Est and 1 spliced mRNA
-tawk '$2>50{$2=$2-50; $3=$3+50;print $0}$2<=50{$3=$3+50;print $0}' pseudoEst5AndMrna.bed > pseudoEst5AndMrna.window.bed
+overlapSelect pseudo${GENE1}Cds.bed pseudoEst5AndMrna.bed pseudoEst5AndMrnaNotShuffle.bed -nonOverlapping
+tawk '$2>200{$2=$2-200; $3=$3+200;print $0}$2<=200{$3=$3+200;print $0}' pseudoEst5AndMrnaNotShuffle.bed > pseudoEst5AndMrna.window.bed
 orfBatch $DB pseudoEst5AndMrna.window.bed pseudoEst5AndMrna.out pseudoEst5AndMrna.gp > borfEst5AndMrna.out
 echo gene-check  -nib-dir $NIB pseudoEst5AndMrna.gp checkEst5AndMrna.rdb
 ~markd/compbio/genefinding/GeneTools/bin/x86_64/opt/gene-check  -genome-seqs $NIB pseudoEst5AndMrna.gp checkEst5AndMrna.rdb
@@ -124,7 +163,13 @@ overlapSelect pseudoEst5AndMrna.bed pseudo5Est100AA.bed pseudoEst5AndMrna100AA.b
 
 #split retros by age
 
-for bed in pseudo${GENE2} pseudoEstMrna.filter pseudoEstAll pseudoExpressed pseudoEst5Mrna pseudoEst5 pseudoEst5AndMrna pseudoEst100AA pseudo5Est100AA pseudoEst5AndMrna100AA pseudo${GENE1}Cds pseudo${GENE1}Cds50 pseudo${GENE2}Cds pseudo${GENE2}Cds50 ; do $SPLITBYAGE ${bed}.bed ${bed}.ancient.bed ${bed}.recent.bed; done
+for bed in pseudo${GENE2} pseudoEstMrna.filter pseudoEstAll pseudoExpressed pseudoEst5Mrna pseudoEst5 pseudoEst5AndMrna pseudoEst100AA pseudo5Est100AA pseudoEst5AndMrna100AA pseudo${GENE1}Cds pseudo${GENE1}Cds50 pseudo${GENE2}Cds pseudo${GENE2}Cds50 pseudoEst5MrnaNotKg pseudoEst10MrnaNotKg pseudoEst5AndMrnaNotKg; do $SPLITBYAGE ${bed}.bed ${bed}.ancient.bed ${bed}.recent.bed; done
+
+mkdir -p age
+cd age
+$SCRIPT/doAge ../../DEF pseudoEst5Mrna
+$SCRIPT/doAge ../../DEF retroMrnaInfoLessZnf 
+$SCRIPT/doAge ../../DEF retroKgCoding 
 
 #retros involved in alt-splicing
 
