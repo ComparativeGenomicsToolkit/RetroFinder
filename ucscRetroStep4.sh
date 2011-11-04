d67d2d4795c78c3bd31b8b8005a98aca17252a47
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2011-11-04 19:44:55 +0000

    echo commands and remove echos

--- ucscRetroStep4.sh
+++ ucscRetroStep4.sh
@@ -2,18 +2,18 @@
 #
 # runs small cluster job to remove overlaps by picking highest scoring retro in each locus
 #
-set -beEu -o pipefail
+set -bevEu -o pipefail
 source $1
 echo "---------------------------------------------------------------------------------------"
 echo "Starting ucscRetroStep4.sh $1 on $HOST- catting output from retro pipeline cluster run"
 echo "---------------------------------------------------------------------------------------"
+cd $OUTDIR
 if [[ -s S1.len ]] ; then
     echo "S1.len exists with `wc -l S1.len` rows"
 else
     echo "please create S1.len from chrom.sizes without random chroms or chrM."
     exit 3
 fi
-cd $OUTDIR
 wc -l run.0/jobList | awk '{print $1}'> jobs.cnt
 pushd $RESULT ; ls pseudoGeneLink[0-9]*.bed | wc -l | awk '{print $1}'> $OUTDIR/jobs.lst
 popd
@@ -27,17 +27,13 @@ fi
 # filter out low scoring hits as defined by retroscore and alignment score to parent gene
 rm -f pseudoGeneLinkSortFilter.bed.gz
 echo catting Sorting and Filtering pseudoGeneLinkSortFilter.bed
-echo "pushd $RESULT ; cat pseudoGeneLink[0-9]*.bed | tawk 'score > 400 && (axtScore > 10000 || axtScore == -1) {OFS="\t";print $0}'| $SCRIPT/removeTandemDups | sort -k1,1 -k2,3n -k4,4 to  $OUTDIR/pseudoGeneLinkSortFilter.bed; "
-pushd $RESULT ; cat pseudoGeneLink[0-9]*.bed | tawk '$5 > 400 && ($14 > 10000 || $14 == -1) {OFS="\t";print $0}'| $SCRIPT/removeTandemDups | sort -k1,1 -k2,3n -k4,4 -T /scratch > $OUTDIR/pseudoGeneLinkSortFilter.bed; #/bin/rm $RESULT/pseudoGeneLink[0-9]*.bed
+pushd $RESULT ; cat pseudoGeneLink[0-9]*.bed | tawk '$5 > 300 && ($14 > 10000 || $14 == -1) {OFS="\t";print $0}'| $SCRIPT/removeTandemDups | sort -k1,1 -k2,3n -k4,4 -T /scratch > $OUTDIR/pseudoGeneLinkSortFilter.bed; #/bin/rm $RESULT/pseudoGeneLink[0-9]*.bed
 popd
-echo "Line count:"
 wc -l pseudoGeneLinkSortFilter.bed
 pushd $OUT
-echo creating pseudo.psl
 cat pseudo[0-9]*.psl > $OUTDIR/pseudo.psl & #;/bin/rm $OUT/pseudo[0-9]*.psl &
 popd
 echo Removing Overlaps
-echo splitting
 RESULTSPLIT=$OUTDIR/resultSplit
 gzip pseudoGeneLinkSortFilter.bed
 rm -rf $RESULTSPLIT
@@ -63,4 +59,4 @@ echo "end of ucscRetroStep4.sh , check parasol status then run ucscRetroStep5.sh
 ssh -T $CLUSTER "cd $OVERLAPDIR ; /parasol/bin/para make jobList "
 popd
 pwd
-#$SCRIPT/ucscRetroStep5.sh $1
+$SCRIPT/ucscRetroStep5.sh $1
