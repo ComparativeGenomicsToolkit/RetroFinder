fe997e8a5abb589b5b1e3eb82dbe9fe4e36b63eb
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2011-11-04 20:15:47 +0000

    add version number to tables and WEBROOT variable for web page generation. Remove hardcoded webpage directory names

--- ucscRetroStep6.sh
+++ ucscRetroStep6.sh
@@ -10,49 +10,83 @@ for i in `echo $SPECIES` ; do echo "make html $i"; done
 cp $DEF $OUTDIR/$EXPDIR
 cd $OUTDIR/$EXPDIR
 #web pages for shuffling events
-mkdir -p $ROOTDIR/retro/shuffle2009
-cp $SCRIPT/header.html $ROOTDIR/retro/shuffle2009/index.html
-echo "<TR><TH>data set</TH>" >> $ROOTDIR/retro/shuffle2009/index.html
-for db in `echo $SPECIES` ; do echo "<TH>$db Ancient</TH><TH>$db Recent</TH>" >> $ROOTDIR/retro/shuffle2009/index.html ; done
-echo "</TR>" >> $ROOTDIR/retro/shuffle2009/index.html
-echo "</THEAD><TBODY>" >> $ROOTDIR/retro/shuffle2009/index.html
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE1}Cds.bed shuffle2009/kgshuffle ${GENE1}Shuffle;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE1}Cds50.bed shuffle2009/kgshuffle50 ${GENE1}50%Shuffle;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}Cds.bed shuffle2009/refseqshuffle ${GENE2}Shuffle;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}Cds50.bed shuffle2009/refseqshuffle50 ${GENE2}50%Shuffle;
-echo "</tbody>" >> $ROOTDIR/retro/shuffle2009/index.html
-echo "</table>" >> $ROOTDIR/retro/shuffle2009/index.html
-echo "</body>" >> $ROOTDIR/retro/shuffle2009/index.html
-echo "</html>" >> $ROOTDIR/retro/shuffle2009/index.html
+SHUFFLEDIR=shuffle
+SHUFFLEROOT=$WEBROOT/$SHUFFLEDIR
+mkdir -p $SHUFFLEROOT
+cp $SCRIPT/header.html $SHUFFLEROOT/index.html
+echo "<TR><TH>data set</TH>" >> $SHUFFLEROOT/index.html
+for db in `echo $SPECIES` ; do echo "<TH>$db Recent</TH><TH>$db Ancient</TH>" >> $SHUFFLEROOT/index.html ; done
+echo "</TR>" >> $SHUFFLEROOT/index.html
+echo "</THEAD><TBODY>" >> $SHUFFLEROOT/index.html
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE1}Cds.bed $SHUFFLEDIR/kgshuffle ${GENE1}Shuffle;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE1}Cds50.bed $SHUFFLEDIR/kgshuffle50 ${GENE1}50Shuffle;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}Cds.bed $SHUFFLEDIR/refseqshuffle ${GENE2}Shuffle;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}Cds50.bed $SHUFFLEDIR/refseqshuffle50 ${GENE2}50%Shuffle;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE1}ProtCds.bed $SHUFFLEDIR/kgshuffleProt ${GENE1}ShuffleProtEvidence;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}ProtCds.bed $SHUFFLEDIR/refseqshuffleProt ${GENE2}ShuffleProtEvidence;
+echo "</tbody>" >> $SHUFFLEROOT/index.html
+echo "</table>" >> $SHUFFLEROOT/index.html
+echo "</body>" >> $SHUFFLEROOT/index.html
+echo "</html>" >> $SHUFFLEROOT/index.html
 
 #web pages for duplication (type2) events
-mkdir -p $ROOTDIR/retro/type2_2009
-cp $SCRIPT/header.html $ROOTDIR/retro/type2_2009/index.html
-echo "<TR><TH>data set</TH>" >> $ROOTDIR/retro/type2_2009/index.html
-for db in `echo $SPECIES` ; do echo "<TH>$db Ancient</TH><TH>$db Recent</TH>" >> $ROOTDIR/retro/type2_2009/index.html ; done
-echo "</TR>" >> $ROOTDIR/retro/type2_2009/index.html
-echo "</THEAD><TBODY>" >> $ROOTDIR/retro/type2_2009/index.html
-for i in Coding Noncoding Nearcoding Antisense ; do $SCRIPT/makeHtmlRightDb.sh $DEF retroKg${i}.bed type2_2009/known${i} known${i}; done
-$SCRIPT/makeHtmlRightDb.sh $DEF ../retroMrnaInfoLessZnf.bed type2_2009/all510 expressedAndNonExpressedScore510;
-$SCRIPT/makeHtmlRightDb.sh $DEF ../retroMrnaInfo650.bed type2_2009/all650 expressedAndNonExpressedScore650;
-#$SCRIPT/makeHtmlRightDb.sh $DEF pseudoExpressedAll.bed type2_2009/expressedAll510 expressedAll510;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEstMrna.filter.bed type2_2009/estMrna atLeastOneEstOrMrna;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEstAll.bed type2_2009/est atLeastOneEst;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoExpressed.bed type2_2009/estOrf atLeastOneEstGoodOrf
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.bed type2_2009/est5Mrna 5EstOrMrna;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5.bed type2_2009/est5 5Est;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5AndMrna.bed type2_2009/5estAndMrna 5EstAndMrna;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst100AA.bed type2_2009/estOrf100 1EstGoodOrfBigger100aa;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudo5Est100AA.bed type2_2009/5estOrf100 5estGoodOrfBigger100aa;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5AndMrna100AA.bed type2_2009/5estAndMrnaOrf100 5estAndMrnaGoodOrfBigger100aa;
-$SCRIPT/makeHtmlRightDb.sh $DEF retroArray.bed type2_2009/gnfAtlas2 gnfAtlas2;
-$SCRIPT/makeHtmlRightDb.sh $DEF retroSplice.bed type2_2009/retroSplice Splice;
-$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}.bed type2_2009/refSeqAll refSeqAll;
-echo "</tbody>" >> $ROOTDIR/retro/type2_2009/index.html
-echo "</table>" >> $ROOTDIR/retro/type2_2009/index.html
-echo "</body>" >> $ROOTDIR/retro/type2_2009/index.html
-echo "</html>" >> $ROOTDIR/retro/type2_2009/index.html
+DUPDIR=dups
+DUPROOT=$WEBROOT/$DUPDIR
+mkdir -p $DUPROOT
+cp $SCRIPT/header.html $DUPROOT/index.html
+echo "<TR><TH>data set</TH>" >> $DUPROOT/index.html
+for db in `echo $SPECIES` ; do echo "<TH>$db Recent</TH><TH>$db Ancient</TH>" >> $DUPROOT/index.html ; done
+echo "</TR>" >> $DUPROOT/index.html
+echo "</THEAD><TBODY>" >> $DUPROOT/index.html
+for i in Coding Noncoding Nearcoding Antisense ; do $SCRIPT/makeHtmlRightDb.sh $DEF retroKg${i}.bed $DUPDIR/known${i} known${i}; done
+$SCRIPT/makeHtmlRightDb.sh $DEF ucscRetroProtein.bed $DUPDIR/SpProtEvidence SpProtEvidence;
+$SCRIPT/makeHtmlRightDb.sh $DEF ucscRetroTranscript.bed $DUPDIR/SpTransEvidence SpTransEvidence;
+$SCRIPT/makeHtmlRightDb.sh $DEF ../retroMrnaInfoLessZnf.bed $DUPDIR/all510 expressedAndNonExpressedScore510;
+$SCRIPT/makeHtmlRightDb.sh $DEF ../retroMrnaInfo650.bed $DUPDIR/all650 expressedAndNonExpressedScore650;
+#$SCRIPT/makeHtmlRightDb.sh $DEF pseudoExpressedAll.bed $DUPDIR/expressedAll510 expressedAll510;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEstMrna.filter.bed $DUPDIR/estMrna atLeastOneEstOrMrna;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEstAll.bed $DUPDIR/est atLeastOneEst;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoExpressed.bed $DUPDIR/estOrf atLeastOneEstGoodOrf
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.bed $DUPDIR/est5Mrna 5EstOrMrna;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5MrnaNotKg.bed $DUPDIR/est5MrnaNotKg 5EstOrMrnaNotKg;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst10Mrna.bed $DUPDIR/est10Mrna 10EstOrMrna;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst10MrnaNotKg.bed $DUPDIR/est10MrnaNotKg 10EstOrMrnaNotKg;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5.bed $DUPDIR/est5 5Est;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5AndMrna.bed $DUPDIR/5estAndMrna 5EstAndMrna;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5AndMrnaNotKg.bed $DUPDIR/5estAndMrnaNotKg 5EstAndMrnaNotKg;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst100AA.bed $DUPDIR/estOrf100 1EstGoodOrfBigger100aa;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo5Est100AA.bed $DUPDIR/5estOrf100 5estGoodOrfBigger100aa;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5AndMrna100AA.bed $DUPDIR/5estAndMrnaOrf100 5estAndMrnaGoodOrfBigger100aa;
+$SCRIPT/makeHtmlRightDb.sh $DEF retroArray.bed $DUPDIR/gnfAtlas2 gnfAtlas2;
+$SCRIPT/makeHtmlRightDb.sh $DEF retroSplice.bed $DUPDIR/retroSplice Splice;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudo${GENE2}.bed $DUPDIR/refSeqAll refSeqAll;
+$SCRIPT/makeHtmlRightDb.sh $DEF top50.bed $DUPDIR/top50 top50;
+echo "</tbody>" >> $DUPROOT/index.html
+echo "</table>" >> $DUPROOT/index.html
 
+echo "</body>" >> $DUPROOT/index.html
+echo "</html>" >> $DUPROOT/index.html
+
+#web pages for age breakdown 
+AGEDIR=age
+AGEROOT=$WEBROOT/$AGEDIR
+mkdir -p $AGEROOT
+cp $SCRIPT/header.html $AGEROOT/index.html
+echo "<TR><TH>data set</TH>" >> $AGEROOT/index.html
+for db in `echo $SPECIES` ; do echo "<TH>$db Recent</TH><TH>$db Ancient</TH>" >> $AGEROOT/index.html ; done
+echo "</TR>" >> $AGEROOT/index.html
+echo "</THEAD><TBODY>" >> $AGEROOT/index.html
+$SCRIPT/makeHtmlRightDb.sh $DEF ucscRetroProtein.bed $AGEDIR/SpProtEvidence SpProtEvidence;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.0ancient.bed $AGEDIR/ancient ancient;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.1mouse.bed $AGEDIR/mouse mouse;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.2rhesus.bed $AGEDIR/rhesus rhesus;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.3ape.bed $AGEDIR/ape ape;
+$SCRIPT/makeHtmlRightDb.sh $DEF pseudoEst5Mrna.4human.bed $AGEDIR/human humanSpecific;
+echo "</tbody>" >> $AGEROOT/index.html
+echo "</table>" >> $AGEROOT/index.html
+
+echo "</body>" >> $AGEROOT/index.html
+echo "</html>" >> $AGEROOT/index.html
 echo " add following entry to trackDb.ra"
 echo ""
 echo "track ucscRetroAli$VERSION"
@@ -66,8 +100,8 @@ echo "visibility pack"
 echo "nextItemButton on"
 echo "retroMrnaInfo ucscRetroInfo$VERSION"
 echo "baseColorDefault diffCodons"
-echo "baseColorUseCds table ucscRetroCds"
-echo "baseColorUseSequence extFile ucscRetroSeq ucscRetroExtFile"
+echo "baseColorUseCds table ucscRetroCds$VERSION"
+echo "baseColorUseSequence extFile ucscRetroSeq$VERSION ucscRetroExtFile$VERSION"
 echo "indelDoubleInsert on"
 echo "indelQueryInsert on"
 echo "showDiffBasesAllScales ."
