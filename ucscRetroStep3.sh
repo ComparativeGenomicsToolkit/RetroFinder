e8eda3b5ddfd72e7862192142868aa6d869cd51d
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2011-11-04 10:57:41 +0000

    copy DEF file to next working dir.  add /dev/null support to DEF variables, NET* and GENE*

--- ucscRetroStep3.sh
+++ ucscRetroStep3.sh
@@ -8,13 +8,14 @@ set -beEu -o pipefail
 source $1
 echo "starting ucscRetroStep3.sh $1 for $DB"
 mkdir -p $OUTDIR
+cp $1 $OUTDIR
 cd $OUTDIR
 mkdir -p result
 mkdir -p result/axt
 mkdir -p log
 mkdir -p out
 
-ls $NIB/*.nib > $OUTDIR/S1.lst
+find $NIB -name \*.nib > $OUTDIR/S1.lst
 cp $GENOME/$DB/chrom.sizes .
 
 #cat $RMSK/*.out |awk '{OFS="\t";print $5,$6,$7}' | grep -v position|grep -v sequence | tawk 'length($0)>2{print $0}' > rmsk.bed
@@ -27,16 +28,32 @@ else
         for i in `cut -f 1 chrom.sizes` ;do hgsql $DB -N -B -e "select genoName , genoStart , genoEnd from ${i}_rmsk" >> rmsk.bed ; done  
     fi
 fi
-hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET1 where tName not like '%hap%'" > \
-$NET1.txt
-hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET2 where tName not like '%hap%'" > \
-$NET2.txt 
-hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET3 where tName not like '%hap%'" > \
-$NET3.txt 
-hgsql $DB -N -B -e "select chrom, chromStart, chromEnd from simpleRepeat" > simpleRepeat.bed 
-hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE1 order by chrom, txStart, txEnd" | sort -k2,2 -k4,4n > $GENE1.tab
-hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE2" > $GENE2.tab 
-hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE3" > $GENE3.tab 
+if [ $NET1 != "/dev/null" ]; 
+then hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET1 where tName not like '%hap%'" > \
+$NET1.txt;
+fi
+if [ $NET2 != "/dev/null" ]; 
+then hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET2 where tName not like '%hap%'" > \
+$NET2.txt ;
+fi
+if [ $NET3 != "/dev/null" ]; 
+then hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET3 where tName not like '%hap%'" > \
+$NET3.txt ;
+fi
+if [[ -s simpleRepeat.bed.gz ]] ; then
+    echo "simpleRepeat.bed.gz not refreshed"
+else
+hgsql $DB -N -B -e "select chrom, chromStart, chromEnd from simpleRepeat" > simpleRepeat.bed ;
+fi
+if [ $GENE1 != "/dev/null" ]; 
+then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE1 order by chrom, txStart, txEnd" | sort -k2,2 -k4,4n > $GENE1.tab;
+fi
+if [ $GENE2 != "/dev/null" ]; 
+then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE2" > $GENE2.tab ;
+fi
+if [ $GENE3 != "/dev/null" ]; 
+then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE3" > $GENE3.tab ;
+fi
 rm -f $GENE1.tab.gz $GENE2.tab.gz $GENE3.tab.gz *.txt.gz *.bed.gz
 gzip *.tab &
 gzip *.txt &
@@ -44,6 +61,8 @@ gzip *.bed
 
 rm -f mrna.2bit
 ln $MRNABASE/mrna.2bit . -s
+ln $MRNABASE/all_mrna.psl.gz . -s
+ln $MRNABASE/cds.tab.gz . -s
 
 ####
 # run retro pipeline on cluster
