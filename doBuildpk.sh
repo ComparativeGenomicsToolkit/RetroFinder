b66e47e57bc151b7612a6669e013f85be97ddf45
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2011-11-05 18:48:35 +0000

    renumber col 34 to col32 after deleting ortho fields form ucscRetroInfo

--- doBuildpk.sh
+++ doBuildpk.sh
@@ -13,7 +13,7 @@ echo "/hive/users/baertsch/bin/x86_64/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/c
 /hive/users/baertsch/bin/x86_64/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt /dev/null $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log
 if [ $? == 0 ]; then
 ulimit -a >> $TMP/pseudo$1.log
- /bin/awk '$34 == 1 || $34==-2 {print $0}' $TMP/pseudoMrnaLink$1.txt > $TMP/pseudoGeneLink$1.bed
+ /bin/awk '$32 == 1 || $32==-2 {print $0}' $TMP/pseudoMrnaLink$1.txt > $TMP/pseudoGeneLink$1.bed
  /bin/gzip $TMP/pseudo$1.log
  /bin/mv $TMP/pseudo$1.log.gz $LOG/pseudo$1.log.gz
  /bin/rm -f $TMP/pseudoMrnaLink$1.txt.gz
