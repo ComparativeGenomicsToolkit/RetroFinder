1074034cfcb1c54c6563a88220991ba52a0b6ed2
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2011-11-07 02:47:40 +0000

    add missing age entries

--- analyseAge.sh
+++ analyseAge.sh
@@ -22,13 +22,13 @@ if [[ $GENE1 == "knownGene" ]]
 then
 for i in ${GENE1}ProtCds ${GENE2}ProtCds; do $SCRIPT/selectById -tsv 1 ortho.ancient.id 4 pseudo${i}.bed |sort -k5,5nr> pseudo${i}.ancient.bed ; $SCRIPT/selectById -not -tsv 1 ortho.ancient.id 4 pseudo${i}.bed |sort -k5,5nr> pseudo${i}.recent.bed ; done
 fi
+for i in ucscRetroProtein ucscRetroTranscript ; do $SCRIPT/selectById -tsv 1 ortho.ancient.id 4 ${i}.bed |sort -k5,5nr> ${i}.ancient.bed ; $SCRIPT/selectById -not -tsv 1 ortho.ancient.id 4 ${i}.bed |sort -k5,5nr> ${i}.recent.bed ; done
 
-for bed in pseudo${GENE2} pseudoEstMrna.filter pseudoEstAll pseudoExpressed pseudoEst5Mrna pseudoEst5 pseudoEst5AndMrna pseudoEst100AA pseudo5Est100AA pseudoEst5AndMrna100AA pseudo${GENE1}Cds pseudo${GENE1}Cds50 pseudo${GENE2}Cds pseudo${GENE2}Cds50 pseudoEst5MrnaNotKg pseudoEst10MrnaNotKg pseudoEst5AndMrnaNotKg; do $SCRIPT/selectById -tsv 1 ortho.ancient.id 4 ${bed}.bed |sort -k5,5nr> ${bed}.ancient.bed ; $SCRIPT/selectById -not -tsv 1 ortho.ancient.id 4 ${bed}.bed |sort -k5,5nr> ${bed}.recent.bed ; done
+for bed in pseudo${GENE2} pseudoEstMrna.filter pseudoEstAll pseudoExpressed pseudoEst5Mrna pseudoEst5 pseudoEst5AndMrna pseudoEst100AA pseudo5Est100AA pseudoEst5AndMrna100AA pseudo${GENE1}Cds pseudo${GENE1}Cds50 pseudo${GENE2}Cds pseudo${GENE2}Cds50 pseudoEst5MrnaNotKg pseudoEst10Mrna pseudoEst10MrnaNotKg pseudoEst5AndMrnaNotKg; do $SCRIPT/selectById -tsv 1 ortho.ancient.id 4 ${bed}.bed |sort -k5,5nr> ${bed}.ancient.bed ; $SCRIPT/selectById -not -tsv 1 ortho.ancient.id 4 ${bed}.bed |sort -k5,5nr> ${bed}.recent.bed ; done
 
 #for i in Coding Noncoding Nearcoding Antisense ; do $SPLITBYAGE retroKg${i}.bed retroKg${i}.ancient.bed retroKg${i}.recent.bed; done
 #TODO: convert these to selectById
 #for i in ${GENE1}Cds ${GENE2}Cds ${GENE1}Cds50 ${GENE2}Cds50 ; do $SPLITBYAGE pseudo${i}.bed pseudo${i}.ancient.bed pseudo${i}.recent.bed ; done
-#for i in ucscRetroProtein ucscRetroTranscript ; do $SPLITBYAGE ${i}.bed ${i}.ancient.bed ${i}.recent.bed ; done
 
 
 if [ -f altSplice.bed ] 
