cf02bbf56a1d59f1a19faf338a61c29159574b97
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2012-08-13 22:10:44 +0000

    Added path variable to program and added line to source DEF file.

--- src/pipeline/countSingleExon.sh
+++ src/pipeline/countSingleExon.sh
@@ -1,4 +1,5 @@
-#
+# Provide DEF file as argument
+source $1
 echo reading gp.list, counting genes with one coding exon
-for i in `cat gp.list` ; do  echo $i ; wc -l $i |awk '{print $1}' ; genePredFilter -cdsExons=2 $i stdout | wc -l ; done > x
+for i in `cat gp.list` ; do  echo $i ; wc -l $i |awk '{print $1}' ; ${BINDIR}/genePredFilter -cdsExons=2 $i stdout | wc -l ; done > x
 awk 'BEGIN{n=0}n==2{printf("%d\t%d\t%4.2f\t%s\n", tot-$1,tot,(tot-$1)/tot,name);n++}n==1{tot=$1;n++}n==0{name= $1;n++}n==3{n=0}' x
