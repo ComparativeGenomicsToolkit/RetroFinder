ac53db03a03edb555f8218a4908af297fc4be668
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2012-10-10 22:09:03 +0000

    fix to use standard overlapSelect

--- src/pipeline/estStat.sh
+++ src/pipeline/estStat.sh
@@ -1,6 +1,6 @@
 #
 CHROM=$1
-overlapSelect $CHROM.psl ../retroMrnaInfoLessZnf.bed pseudoEst.$CHROM.bed
-overlapSelect $CHROM.psl ../retroMrnaInfoLessZnf.bed -idOutput stdout |awk '{print $1}' | sort |uniq> est.$CHROM.id
-overlapSelect $CHROM.psl ../retroMrnaInfoLessZnf.bed -statsOutput stdout | sort > stat.$CHROM.out
-overlapSelect $CHROM.psl ../retroMrnaInfoLessZnf.bed -statsOutput -aggregate stdout | sort > statagg.$CHROM.out
+overlapSelect -inCoorCols=chromCol,startCol,endCol,strandCol,name $CHROM.psl ../retroMrnaInfoLessZnf.bed pseudoEst.$CHROM.bed
+overlapSelect -inCoorCols=chromCol,startCol,endCol,strandCol,name $CHROM.psl ../retroMrnaInfoLessZnf.bed -idOutput stdout |awk '{print $1}' | sort |uniq> est.$CHROM.id
+overlapSelect -inCoorCols=chromCol,startCol,endCol,strandCol,name $CHROM.psl ../retroMrnaInfoLessZnf.bed -statsOutput stat.$CHROM.out
+overlapSelect -inCoorCols=chromCol,startCol,endCol,strandCol,name $CHROM.psl ../retroMrnaInfoLessZnf.bed -statsOutput -aggregate statagg.$CHROM.out
