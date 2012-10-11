021b49d51d244b3e0049ac856f4bdb55e73e64da
baertsch <baertsch@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2012-10-11 00:33:49 +0000

    fix overlapSelect

--- src/pipeline/compareKaess.sh
+++ src/pipeline/compareKaess.sh
@@ -3,10 +3,10 @@ DEF=$1
 INPUT=$2
 DIR=$3
 NAME=$4
-overlapSelect pnasretroExp.from.hg17.bed $INPUT kassMatch.$INPUT 
-overlapSelect pnasretroExp.from.hg17.bed $INPUT kassMissed.$INPUT -nonOverlapping
-overlapSelect $INPUT pnasretroExp.from.hg17.bed temp.bed -nonOverlapping
-overlapSelect shuffleGood.bed temp.bed Missed.$INPUT -nonOverlapping
+overlapSelect -inCoordCols=0,1,2,5,3 pnasretroExp.from.hg17.bed $INPUT kassMatch.$INPUT 
+overlapSelect -inCoordCols=0,1,2,5,3 pnasretroExp.from.hg17.bed $INPUT kassMissed.$INPUT -nonOverlapping
+overlapSelect -inCoordCols=0,1,2,5,3 $INPUT pnasretroExp.from.hg17.bed temp.bed -nonOverlapping
+overlapSelect -inCoordCols=0,1,2,5,3 shuffleGood.bed temp.bed Missed.$INPUT -nonOverlapping
 makeHtmlRight.sh $DEF kassMatch.$INPUT $DIR $NAME.Kass
 makeHtmlRight.sh $DEF Missed.$INPUT $DIR.miss $NAME.Kass.miss
 wc -l $INPUT kassMatch.$INPUT kassMissed.$INPUT
