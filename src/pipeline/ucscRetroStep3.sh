6b01b02561be5666cd183024df7de871f8bd1a6c
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2014-05-02 20:45:40 +0000

    Added absolute paths.

--- src/pipeline/ucscRetroStep3.sh
+++ src/pipeline/ucscRetroStep3.sh
@@ -7,78 +7,77 @@
 set -bevEu -o pipefail
 source $1
 echo "starting ucscRetroStep3.sh $1 for $DB"
-mkdir -p $OUTDIR
+# $OUTDIR was already created by ucscRetroStep2.sh
 cp $1 $OUTDIR
-cd $OUTDIR
-mkdir -p result
-mkdir -p result/axt
-mkdir -p log
-mkdir -p out
+# cd $OUTDIR
+mkdir -p $OUTDIR/result
+mkdir -p $OUTDIR/result/axt
+mkdir -p $OUTDIR/log
+mkdir -p $OUTDIR/out
 
 find $NIB -name \*.nib > $OUTDIR/S1.lst
-cp $GENOME/$DB/chrom.sizes .
+cp $GENOME/$DB/chrom.sizes $OUTDIR
 
 #cat $RMSK/*.out |awk '{OFS="\t";print $5,$6,$7}' | grep -v position|grep -v sequence | tawk 'length($0)>2{print $0}' > rmsk.bed
-if [[ -s rmsk.bed.gz ]] ; then
-    echo "rmsk.bed.gz not refreshed"
+if [[ -s $OUTDIR/rmsk.bed.gz ]] ; then
+    echo "$OUTDIR/rmsk.bed.gz not refreshed"
 else
-    rm -f rmsk.bed rmsk.bed.gz
-    if [ $RMSK == "rmsk" ]; then hgsql $DB -N -B -e "select genoName , genoStart , genoEnd from rmsk" >> rmsk.bed ;
+    rm -f $OUTDIR/rmsk.bed
+    if [ $RMSK == "rmsk" ]; then hgsql $DB -N -B -e "select genoName , genoStart , genoEnd from rmsk" >> $OUTDIR/rmsk.bed ;
     else 
-        for i in `cut -f 1 chrom.sizes` ;do hgsql $DB -N -B -e "select genoName , genoStart , genoEnd from ${i}_rmsk" >> rmsk.bed ; done  
+        for i in `cut -f 1 chrom.sizes` ;do hgsql $DB -N -B -e "select genoName , genoStart , genoEnd from ${i}_rmsk" >> $OUTDIR/rmsk.bed ; done  
     fi
 fi
 if [ $NET1 != "/dev/null" ]; 
 then hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET1 where tName not like '%hap%'" > \
-$NET1.txt;
+$OUTDIR/$NET1.txt;
 fi
 if [ $NET2 != "/dev/null" ]; 
 then hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET2 where tName not like '%hap%'" > \
-$NET2.txt ;
+$OUTDIR/$NET2.txt ;
 fi
 if [ $NET3 != "/dev/null" ]; 
 then hgsql $DB -N -B -e "select tName, tStart, tEnd, level, qName, qStart, qEnd, type, qN, tR+tTrf from $NET3 where tName not like '%hap%'" > \
-$NET3.txt ;
+$OUTDIR/$NET3.txt ;
 fi
-if [[ -s simpleRepeat.bed.gz ]] ; then
-    echo "simpleRepeat.bed.gz not refreshed"
+if [[ -s $OUTDIR/simpleRepeat.bed.gz ]] ; then
+    echo "$OUTDIR/simpleRepeat.bed.gz not refreshed"
 else
-hgsql $DB -N -B -e "select chrom, chromStart, chromEnd from simpleRepeat" > simpleRepeat.bed ;
+hgsql $DB -N -B -e "select chrom, chromStart, chromEnd from simpleRepeat" > $OUTDIR/simpleRepeat.bed ;
 fi
 if [ $GENE1 != "/dev/null" ]; 
-then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE1 order by chrom, txStart, txEnd" | sort -k2,2 -k4,4n > $GENE1.tab;
+then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE1 order by chrom, txStart, txEnd" | sort -k2,2 -k4,4n > $OUTDIR/$GENE1.tab;
 fi
 if [ $GENE2 != "/dev/null" ]; 
-then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE2" > $GENE2.tab ;
+then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE2" > $OUTDIR/$GENE2.tab ;
 fi
 if [ $GENE3 != "/dev/null" ]; 
-then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE3" > $GENE3.tab ;
+then hgsql $DB -N -B -e "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from $GENE3" > $OUTDIR/$GENE3.tab ;
 fi
-rm -f $GENE1.tab.gz $GENE2.tab.gz $GENE3.tab.gz *.txt.gz *.bed.gz
-gzip *.tab &
-gzip *.txt &
-gzip *.bed &
+rm -f $OUTDIR/$GENE1.tab.gz $OUTDIR/$GENE2.tab.gz $OUTDIR/$GENE3.tab.gz $OUTDIR/*.txt.gz $OUTDIR/*.bed.gz
+gzip $OUTDIR/*.tab &
+gzip $OUTDIR/*.txt &
+gzip $OUTDIR/*.bed &
 
-rm -f mrna.2bit
-ln $MRNABASE/mrna.2bit . -s
-ln $MRNABASE/all_mrna.psl.gz . -s
-ln $MRNABASE/cds.tab.gz . -s
+rm -f $OUTDIR/mrna.2bit
+ln $MRNABASE/mrna.2bit $OUTDIR -s
+ln $MRNABASE/all_mrna.psl.gz $OUTDIR -s
+ln $MRNABASE/cds.tab.gz $OUTDIR -s
 
 ####
 # run retro pipeline on cluster
 ####
-cd $OUTDIR
-mkdir -p run.0
-cd run.0
-wc -l ../split/*.psl |grep -v total | grep -v acc.lst| sort -nr | awk '{print $2}' |sed -e 's,../split/tmp,,'|sed -e 's/.psl//' > list
-cp $OUTDIR/$1 .
-echo "#LOOP" > gsub
-echo "$SCRIPT/doBuildpk.sh \$(path1) $OUTDIR/$1 {check out exists $RESULT/pseudoGeneLink\$(path1).bed} " >> gsub
-echo "#ENDLOOP" >> gsub
+# cd $OUTDIR
+mkdir -p $OUTDIR/run.0
+# cd run.0
+wc -l $OUTDIR/split/*.psl |grep -v total | grep -v acc.lst| sort -nr | awk '{print $2}' |sed -e 's,$OUTDIR/split/tmp,,'|sed -e 's/.psl//' > $OUTDIR/run.0/list
+cp $OUTDIR/$1 $OUTDIR/run.0
+echo "#LOOP" > $OUTDIR/run.0/gsub
+echo "$SCRIPT/doBuildpk.sh \$(path1) $OUTDIR/$1 {check out exists $RESULT/pseudoGeneLink\$(path1).bed} " >> $OUTDIR/run.0/gsub
+echo "#ENDLOOP" >> $OUTDIR/run.0/gsub
 
-gensub2 list single gsub jobList
+gensub2 $OUTDIR/run.0/list single $OUTDIR/run.0/gsub $OUTDIR/run.0/jobList
 echo "Job Count"
-wc -l jobList
+wc -l $OUTDIR/run.0/jobList
 ssh -T $CLUSTER "cd $OUTDIR/run.0 ; /parasol/bin/para make jobList ram=4g"
 echo "check parasol status and then run ucscRetroStep4.sh DEF"
-cd ..
