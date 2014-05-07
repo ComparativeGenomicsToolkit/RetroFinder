e3c67dd2e0290157c13b46fb867a13233dbf954f
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2014-05-07 21:17:56 +0000

    Added extra input which is location of lastz program. Added code to save axt files as were needed for another project.

--- src/pipeline/lastz.sh
+++ src/pipeline/lastz.sh
@@ -4,10 +4,13 @@ TARGET=$1
 QUERY=$2
 COV=$3
 ID=$4
-TMPOUT=$5.$(hostname)..$$tmp
 OUT=$5
-S1=$6
-S2=$7
+TMPOUT=$5.$(hostname)..$$tmp
+AXTOUT=$6
+S1=$7
+S2=$8
+# Location of lastz program
+LASTZ=$9
 FILE=${OUT%.*}
 CHROM=${OUT%/*}
 LAST=${OUT##*/}
@@ -18,7 +21,11 @@ mkdir -p /scratch/tmp/retro/$MID
 #echo mkdir -p /scratch/tmp/retro/$MID >> lastz.log
 #pwd >> lastz.log
 #uname -n >> lastz.log
-/cluster/bin/penn/x86_64/lastz $TARGET $QUERY --hspthresh=2000 --format=axt > $AXT
+echo "AXT: $AXT AXTOUT: $AXTOUT"
+$LASTZ $TARGET $QUERY --hspthresh=2000 --format=axt > $AXT
 axtToPsl $AXT $S1 $S2 $TMPOUT
 mv $TMPOUT $OUT
-rm -f $AXT
+echo "Making directory $AXTOUT"
+mkdir -p $AXTOUT
+#rm -f $AXT
+mv $AXT $AXTOUT
