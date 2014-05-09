3abb4f04d91c60b8a863ca44f87767ed2508e8e5
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2014-05-09 16:51:41 +0000

    Added absolute paths instead of changing directories.

--- src/pipeline/filterMrna.sh
+++ src/pipeline/filterMrna.sh
@@ -5,17 +5,17 @@
 set -beEu -o pipefail
 source $1
 echo "starting filterMrna.sh $1 for $DB"
-tawk '{print $10}' $MRNABASE/gbMrnaOnly.psl |sort |uniq > all_mrna.list
-cat $MRNABASE/gbMrnaOnly.psl |sort -k10,10> all_mrna.qName.psl
-pslStats -queryStats all_mrna.qName.psl all_mrna.stats 
-awk 'NF > 1 && $3 > 1{print $1}' all_mrna.stats  > all_mrna.doublehit.list
+tawk '{print $10}' $MRNABASE/gbMrnaOnly.psl |sort |uniq > $OUTDIR/all_mrna.list
+cat $MRNABASE/gbMrnaOnly.psl |sort -k10,10> $OUTDIR/all_mrna.qName.psl
+pslStats -queryStats $OUTDIR/all_mrna.qName.psl $OUTDIR/all_mrna.stats 
+awk 'NF > 1 && $3 > 1{print $1}' $OUTDIR/all_mrna.stats > $OUTDIR/all_mrna.doublehit.list
 #awk 'NF > 1 && $3 == 1{print $1}' all_mrna.stats  > all_mrna.singlehit.list
-pslSelect -queries=all_mrna.doublehit.list all_mrna.qName.psl stdout | grep -v "_hap" |grep -v random |sort -k10,10> all_mrna_double.sort.psl
-cut -f 10 all_mrna_double.sort.psl |sort |uniq > all_mrna.doublehit.list
-$SCRIPT/selectById -not 1 all_mrna.doublehit.list 10 $MRNABASE/gbMrnaOnly.psl > all_mrna_kept.psl
-faToTwoBit -stripVersion $MRNABASE/mrna.fa mrnaNoversion.2bit
+pslSelect -queries=$OUTDIR/all_mrna.doublehit.list $OUTDIR/all_mrna.qName.psl stdout | grep -v "_hap" |grep -v random |sort -k10,10> $OUTDIR/all_mrna_double.sort.psl
+cut -f 10 $OUTDIR/all_mrna_double.sort.psl |sort |uniq > $OUTDIR/all_mrna.doublehit.list
+$SCRIPT/selectById -not 1 $OUTDIR/all_mrna.doublehit.list 10 $MRNABASE/gbMrnaOnly.psl > $OUTDIR/all_mrna_kept.psl
+faToTwoBit -stripVersion $MRNABASE/mrna.fa $OUTDIR/mrnaNoversion.2bit
 
-pslCDnaGenomeMatch all_mrna_double.sort.psl S1.len mrnaNoversion.2bit /hive/data/genomes/$DB/nib all_mrna_double.filter.psl -score=all_mrna_double.score -bedOut=all_mrna_double.bed  -verbose=3 -minDiff=4 -notAlignPenalty=3 > all.log 
-cat all_mrna_kept.psl all_mrna_double.filter.psl > all_mrnaFiltered.psl
-gzip all_mrnaFiltered.psl
+pslCDnaGenomeMatch $OUTDIR/all_mrna_double.sort.psl $OUTDIR/S1.len $OUTDIR/mrnaNoversion.2bit /hive/data/genomes/$DB/nib $OUTDIR/all_mrna_double.filter.psl -score=$OUTDIR/all_mrna_double.score -bedOut=$OUTDIR/all_mrna_double.bed  -verbose=3 -minDiff=4 -notAlignPenalty=3 > $OUTDIR/all.log 
+cat $OUTDIR/all_mrna_kept.psl $OUTDIR/all_mrna_double.filter.psl > $OUTDIR/all_mrnaFiltered.psl
+gzip $OUTDIR/all_mrnaFiltered.psl
 
