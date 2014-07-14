b43facd13ea7ce65763256d50674d1bcf3cec62e
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2014-07-14 17:58:33 +0000

    Added /data/tmp variable for temp directory. Added iupac code usage to lastz parameters.

--- src/pipeline/lastz.sh
+++ src/pipeline/lastz.sh
@@ -16,13 +16,14 @@ CHROM=${OUT%/*}
 LAST=${OUT##*/}
 MID=${CHROM##*/}
 #echo MID $MID LAST $LAST $MID/$LAST
-AXT=/scratch/tmp/retro/$MID/$LAST.axt
-mkdir -p /scratch/tmp/retro/$MID
-#echo mkdir -p /scratch/tmp/retro/$MID >> lastz.log
+# $TMPDIR is an environment variable that points at /data/tmp
+AXT=$TMPDIR/retro/$MID/$LAST.axt
+mkdir -p $TMPDIR/retro/$MID
+#echo mkdir -p $TMPDIR/retro/$MID >> lastz.log
 #pwd >> lastz.log
 #uname -n >> lastz.log
 echo "AXT: $AXT AXTOUT: $AXTOUT"
-$LASTZ $TARGET $QUERY --hspthresh=2000 --format=axt > $AXT
+$LASTZ $TARGET $QUERY --ambiguous=iupac --hspthresh=2000 --format=axt > $AXT
 axtToPsl $AXT $S1 $S2 $TMPOUT
 mv $TMPOUT $OUT
 echo "Making directory $AXTOUT"
