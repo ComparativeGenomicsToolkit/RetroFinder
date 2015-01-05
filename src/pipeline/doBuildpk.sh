1058cb280ad90bb65bf1e33a7ae95f7986eebee1
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2015-01-05 21:27:13 +0000

    Added an axt file name as input to pslPseudo as using /dev/null means it is not created.

--- src/pipeline/doBuildpk.sh
+++ src/pipeline/doBuildpk.sh
@@ -11,19 +11,20 @@ SEQ=$1
 source $2
 CACHE=$OUTDIR
 # TMP=/scratch/tmp/retro.$$.$USER
-# $TMPDIR is an environment variable that points to /data/tmp
+# $TMPDIR is an environment variable that points to /data/tmp on hgwdev
+# and /scratch/temp on the ku cluster.
 TMP=$TMPDIR/retro.$$.$USER
 mkdir -p $TMP
-cd $TMP
-rm -f $LOG/pseudo$1.log
+# cd $TMP
+rm -f $TMP/$LOG/pseudo$1.log
 #ulimit -d 2800000
 #ulimit -v 2800000
-#echo "${BINDIR}/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log"
-echo "/hive/users/hartera/GencodeWG/retroFinder/trunk/bin2/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log"
+#echo "${BINDIR}/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $TWOBIT $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log"
+echo "${BINDIR}/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $TWOBIT $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log"
 #${BINDIR}/pslPseudo $RETRO_OPTIONS -verbose=5 -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt /dev/null $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log
 # Run pslPseudo to score alignments using annotations and compile information
 # for ucscRetroInfoXX table.
-/hive/users/hartera/GencodeWG/retroFinder/trunk/bin2/pslPseudo $RETRO_OPTIONS -verbose=5 -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt /dev/null $CACHE/S1.lst $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log
+${BINDIR}/pslPseudo $RETRO_OPTIONS -cdsFile=$CACHE/cds.tab.gz $DB $CACHE/split/tmp$1.psl $CACHE/chrom.sizes $CACHE/rmsk.bed.gz $CACHE/$NET1.txt.gz $CACHE/$NET2.txt.gz $CACHE/simpleRepeat.bed.gz $CACHE/all_mrna.psl.gz $OUT/mrna$1.psl $OUT/pseudo$1.psl $TMP/pseudoMrnaLink$1.txt $TMP/pseudo$1.axt $TWOBIT $CACHE/mrna.2bit $CACHE/$GENE2.tab.gz $CACHE/$GENE3.tab.gz $CACHE/$GENE1.tab.gz $CACHE/$NET3.txt.gz $OUT/ortho$1.txt > $TMP/pseudo$1.log
 if [ $? == 0 ]; then
 ulimit -a >> $TMP/pseudo$1.log
 # For each line of pseudoMrnaLink*.txt, if field 32 is 1 or -2, then print 
@@ -49,7 +50,7 @@ ulimit -a >> $TMP/pseudo$1.log
  /bin/gzip $TMP/pseudo$1.axt
 # Move the pseudo*.axt file to the result/axt directory.
  /bin/mv $TMP/pseudo$1.axt.gz $RESULT/axt
-# /bin/rm -rf $TMP
+ /bin/rm -rf $TMP
 
 
 # /bin/awk '{OFS="	";print $14,$16,$17,$10,$1*3-$2,$9}' pseudoMrna.psl > pseudoMrna.txt
