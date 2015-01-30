e9556ab1a7bf8085d79e54ab9ffb66bb03f87878
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2015-01-30 22:34:21 +0000

    Classes for objects to hold file names for various modules of the pipeline.

---
+++ src/pipeline/bin/Files.py
@@ -0,0 +1,32 @@
+import os, sys, re, subprocess
+import time
+from commonOps import *
+
+# NOTE: Master script needs to get date once so doesn't change if run scripts on multiple days 
+def createRootDirName(outDir, database, date):
+    return  outDir + "/" + database + "." + date
+
+def createWorkingDirName(rootDir, dirPath):
+    return rootDir + "/" + dirPath
+
+class SeqFiles(object):
+    def __init__(self, rootDir, database, seqType, genePred=False):
+        # Database name for genome assembly
+        self.database = database
+        # Sequence type e.g. mrna, refseq, ensembl
+        self.seqType = seqType
+        self.seqsDir = createWorkingDirName(rootDir, "sequenceData")
+        # Names fo files for sequence-related data
+        self.seqFile = createFilePath(self.seqsDir, seqType, "fa")
+        print "seq file is", self.seqFile
+        self.chromFile = createFilePath(self.seqsDir, "chrom", "sizes")
+        # This can be set to None, only required for some datasets
+        # It is converted to PSL later
+        if (genePred):
+            self.genePredFile = createFilePath(self.seqsDir, seqType, "gp")
+        else:
+            self.genePredFile = None
+        # Alignment format is PSL (.psl)
+        self.alignFile = createFilePath(self.seqsDir, seqType, "psl")
+        print "seqFile: %s alignFile: %s genePredFile: %s \n" % (self.seqFile, self.alignFile, self.genePredFile)
+        
