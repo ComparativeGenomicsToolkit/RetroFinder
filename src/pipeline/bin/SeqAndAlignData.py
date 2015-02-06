e60c874a581f67875a140f72e93df76172a9a416
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2015-02-06 18:43:08 +0000

    Changed inputs fro some function calls. fixed bugs.

--- src/pipeline/bin/SeqAndAlignData.py
+++ src/pipeline/bin/SeqAndAlignData.py
@@ -1,7 +1,7 @@
 import os, sys, re, subprocess
 import time
 from commonOps import *
-from Files import SeqFiles
+from Files import SeqFiles,GeneralFiles
 
 # Fetches Genbank and RefSeq sequences
 genbankSeqProg = "/cluster/data/genbank/bin/x86_64/gbGetSeqs"
@@ -10,21 +10,22 @@ gbRoot = "/cluster/data/genbank"
 ensemblSeqProg = "/hive/users/hartera/GencodeWG/retroFinder/trunk/src/pipeline/getEnsemblTxSeqsWithVersions.pl"
 # Converts genePred to PSL format
 gpToPsl = "genePredToPsl"
-# TEMP VARIABLE FOR TESTING
+# Gets cDNA sequence from a genePred table or file
+getRnaProg = "getRnaPred"
 seqType = "mrna"
 
 class SeqAndAlignData(object):
-    def __init__(self, database, alignTable, generalFiles, seqFiles):
+    def __init__(self, database, alignTable, seqFiles, chromFile=None, ensembl=False, ensDb=None):
         self.database = database
         self.seqFiles = seqFiles
-        # Get the PSL alignment data for this dataset
-        self.__getPslAlignments(alignTable)
-        if seqFiles.genePred:
-            self.getPslFromGenePreds(alignTable, self.seqFiles.genePredFile, generalFiles.chromFile)
-        print "seqFile: %s alignFile: %s genePredFile: %s \n" % (self.seqFiles.seqFile, self.seqFiles.alignFile, self.seqFiles.genePredFile)
-        # have an if statement to get Ensembl sequences, do this from 
-        # getEnsemblData
-       #  self.getEnsemblSeqs(self.ensSeqFile)
+        self.ensembl = ensembl
+        self.ensDb = ensDb
+        # Get the PSL alignment data for this dataset and for genePred format
+        # data, also get the genePred file
+        if not self.seqFiles.genePred:
+            self.__getPslAlignments(alignTable)
+        else:
+            self.getPslFromGenePreds(alignTable, self.seqFiles.genePredFile, chromFile)
 
     def __getDatabasePrefix(self):
         """Gets the letters of the prefix of the database"""
@@ -50,6 +51,12 @@ class SeqAndAlignData(object):
         org = getOrganismName(self.database)
         subprocess.check_call([ensemblSeqProg, "--all", org, "all", outFile])
           
+    def getSeqsFromGenePred(self, table, outFile):
+        """Gets the sequences from a table or from a file in genePred format"""
+        makeDir(self.seqFiles.seqsDir)  
+        # table can be a table or a file
+        subprocess.check_call([getRnaProg, self.database, table, "all", outFile])
+
     def __getPslAlignments(self, alignTable):
         """Gets PSL alignments from the database""" 
         makeDir(self.seqFiles.seqsDir)  
@@ -57,18 +64,23 @@ class SeqAndAlignData(object):
         with open(self.seqFiles.alignFile, "w") as fh:
             subprocess.check_call(["hgsql", "-Ne", pslSelect, self.database], stdout=fh)
 
-    def __getGenePredAnnots(self, gpTable):
+    def __getGenePredAnnots(self, gpTable, gpFile):
         """Gets genePred annotations from the database"""
-        genePredSelect = "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds"
-        with open(self.seqFiles.genePredFile, "w") as fh:
+        makeDir(self.seqFiles.seqsDir) 
+        genePredSelect = "select name, chrom, strand, txStart, txEnd, cdsStart, cdsEnd, exonCount, exonStarts, exonEnds from " + gpTable + ";"
+        with open(gpFile, "w") as fh:
             subprocess.check_call(["hgsql", "-Ne", genePredSelect, self.database], stdout=fh)
-     
+        if self.ensembl:
+             # This program gets the version numbers for ids and re-writes 
+             # the genePred with the ids with version numbers.
+             subprocess.check_call(["./getEnsDataWithVersions", self.database, self.ensDb, self.seqFiles.genePredFile, self.seqFiles.seqsDir])
+    
     def __convertGenePredToPsl(self, gpFile, chromFile):
         """Converts annotation data from genePred to PSL format."""
         # NOTE: need to know location of chromFile
-        makeDir(self.seqFiles.seqsDir)  
-        subprocess.check_call(gpToPsl, chromFile, gpFile, self.seqFiles.pslFile)
+        makeDir(self.seqFiles.seqsDir) 
+        subprocess.check_call([gpToPsl, chromFile, gpFile, self.seqFiles.alignFile])
    
     def getPslFromGenePreds(self, gpTable, gpFile, chromFile):
-        self.__getGenePredAnnots(gpTable)
-        self.__convertGenePredToPsl(gpTable, goFile, chromFile)
+        self.__getGenePredAnnots(gpTable, gpFile)
+        self.__convertGenePredToPsl(gpFile, chromFile)
