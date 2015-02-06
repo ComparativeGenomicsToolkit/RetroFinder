8da5008933d0e2e7486bb04e398b570c6f6f6ce8
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2015-02-06 18:34:05 +0000

    Added functions for reading and storing tab-separated file and function to rename file/dir.

--- src/pipeline/bin/commonOps.py
+++ src/pipeline/bin/commonOps.py
@@ -13,6 +13,9 @@ def catFiles(outFile, fileList):
     with open(outFile, "w") as outFh:
         subprocess.check_call(["cat", fileStr], stdout=outFh)
 
+def renameFile(oldFile, newFile):
+    subprocess.check_call(["mv", oldFile, newFile])
+
 def getDate():
     return time.strftime("%Y-%m-%d") 
 
@@ -51,3 +54,40 @@ def getIdVersion(acc):
     if i: 
         version = i.group(1)
     return version
+
+class TabFileTbl(object):
+    """Read from a tabbed file and store in a dictionary keyed by specified
+       column."""
+    def __init__(self, tabFile, keyCol):
+         self.tabFile = tabFile
+         # Dictonary of rows from tabbed file
+         self.data = None
+         # index (0-based) of column to use as hash key
+         self.keyCol = keyCol
+         self.__buildDictionary()
+
+    def __buildDictionary(self):
+        """Create the dictionary with unique keys"""
+        self.data = dict()
+        for row in TabFileReader(self.tabFile):
+            if row[self.keyCol] in self.data:
+                raise Exception("This value is already in index: " + row[self.keyCol])
+            self.data[row[self.keyCol]] = row
+    
+class TabFileReader(object):
+    """Read from a tabbed file"""
+    def __init__(self, tabFile):
+        self.fh = open(tabFile, "r");
+
+    def __iter__(self):
+        return self
+ 
+    def next(self):
+        while True:
+            line = self.fh.readline()
+            if (line == ""):
+                self.fh.close
+                raise StopIteration
+            if not ((len(line) == 1) or line.startswith('#')):
+                line = line[0:-1]  # drop newline
+                return line.split("\t")
