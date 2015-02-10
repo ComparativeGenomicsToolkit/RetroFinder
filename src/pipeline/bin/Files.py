6cc3118133bfee937662ac73a0d2704c9575b5df
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2015-02-10 02:50:12 +0000

    Added CDS file and updated variable.

--- src/pipeline/bin/Files.py
+++ src/pipeline/bin/Files.py
@@ -15,7 +15,7 @@ class GeneralFiles(object):
         self.chromFile = createFilePath(rootDir, "chrom", "sizes")
 
 class SeqFiles(object):
-    def __init__(self, rootDir, database, seqType, genePred=False):
+    def __init__(self, rootDir, database, seqType, isGenePred=False):
         # Database name for genome assembly
         self.database = database
         # Sequence type e.g. mrna, refseq, ensembl
@@ -27,12 +27,14 @@ class SeqFiles(object):
         # This can be set to None, only required for some datasets
         # It is converted to PSL later
         # Flag to indicate whether this is a genePred annotation
-        self.genePred = genePred
-        if (self.genePred):
+        self.isGenePred = isGenePred
+        if (self.isGenePred):
             self.genePredFile = createFilePath(self.seqsDir, seqType, "gp")
         else:
             self.genePredFile = None
         # Alignment format is PSL (.psl)
         self.alignFile = createFilePath(self.seqsDir, seqType, "psl")
         print "seqFile: %s alignFile: %s genePredFile: %s \n" % (self.seqFile, self.alignFile, self.genePredFile)
-        
+        # CDS regions file
+        cdsStr = "cds." + seqType
+        self.cdsFile = createFilePath(self.seqsDir, cdsStr, "tab") 
