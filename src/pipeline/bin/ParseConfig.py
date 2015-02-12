3aded993dbc1ec8617512d819bf6be8cdd1aa011
hartera <hartera@a8237c23-d22b-0410-ac1a-db40f18c2caa>
2015-02-12 03:59:52 +0000

    added functions to return values of variables from different sections of the config file.

--- src/pipeline/bin/ParseConfig.py
+++ src/pipeline/bin/ParseConfig.py
@@ -3,22 +3,78 @@ from commonOps import *
 
 configFile="configFile.cfg"
 class ParseConfig:
-    def __init__(self, database):
+    def __init__(self, database, configFile):
+        # Read in config file
         self.config = ConfigParser.ConfigParser()
         self.config.read(configFile)
         self.db = database
         self.date = getDate()
+        # root of the directory for this RetroFinder pipeline run
         self.rootRunDir = self.createRootRunDirName()
-        print "Root run directory is", self.rootRunDir
-
-    def writeConfig(self, cfgFile):
-        """Writing our configuration file to configFile.cfg"""
-        with open(cfgFile, 'a') as cfh:
-            self.config.write(cfh)
+        # file for chromosome sizes
+        self.chromFile = self.createPath(self.rootRunDir, \
+            self.getGenVar('chromFile'))  
 
     def createRootRunDirName(self):
-        rootWorkDir = self.config.get('General', 'rootWorkDir', 0) 
+        """Creates path for directory for RetroFinder run"""
+        rootWorkDir = self.getGenVar('rootWorkDir') 
         return  rootWorkDir + "/" + self.db + "/" + self.date
    
-    def createWorkingDirName(self, rootDir, dirPath):
-        return rootDir + "/" + dirPath
+    def createWorkingDirName(self, dirPath):
+        """Creates path and name of directory in root dir"""
+        return self.createPath(self.rootRunDir, dirPath)
+
+    def createPath(self, dir, fileOrDir):
+        """Adds file or directory to path name to create a full file
+           or directory path"""
+        return dir + "/" + fileOrDir
+
+    def getVar(self, section, var):
+        """Returns variable value from config file"""
+        return self.config.get(section, var, 0) 
+
+    def getGenVar(self, var):
+        """Returns variable value from General section of config file"""
+        return self.getVar('General', var)
+
+    def getProgVar(self, var):    
+        """Returns variable value from Programs section of config file"""
+        return self.getVar('Programs', var)
+
+    def getSeqVar(self, var):
+        """Returns variable value from SequenceData section of config file"""
+        return self.getVar('SequenceData', var)
+
+    def getSeqDir(self):
+        """Returns the full path of the sequences directory"""
+        seqDir = createWorkingDirName(self.rootRunDir, \
+            self.getSeqVar('seqDir'))
+        return seqDir
+
+    def getFileName(self, seqType, ext):
+        """Returns file name for sequence-related data""" 
+        return self.getSeqVar(seqType) + "." + ext
+
+    def getSeqFile(self, seqType):
+        """Returns full path and file name of FASTA file for sequences"""
+        # seqType can be mrna, refSeq, ensembl, all or anther type    
+        return createPath(self.getSeqDir(), self.getFileName(seqType, "fa"))
+ 
+    def getPslFile(self, seqType):
+        """Returns full path and file name of PSL file for sequences"""
+        # seqType can be mrna, refSeq, ensembl, all or anther type    
+        return createPath(self.getSeqDir(), self.getFileName(seqType, "psl"))
+    
+    def getGenePredFile(self, seqType):
+        """Returns full path and file name of genePred file for sequences"""
+        # seqType can be ensembl or other genePred annotation
+        return createPath(self.getSeqDir(), self.getFileName(seqType, "gp"))
+
+    def getCdsFile(self, seqType):
+        """Returns full path and file name for CDS regions file"""
+        # seqType can be mrna, refSeq, ensembl, all or anther type    
+        return createPath(self.getSeqDir(), self.getFileName(seqType, "cds.tab"))
+
+    def getChromFile(self, dirPath)
+        return createFilePath(self.rootRunDir, \
+           self.getSeqVar('chromFile'))
